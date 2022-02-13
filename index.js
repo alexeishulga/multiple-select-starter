9e723a7f97990667ee545a031f5e9827ea19fae2
alexeishulga <alexeishulg@gmail.com>
2022-02-14 00:53:41 +0300

    fix

--- index.js
+++ index.js
@@ -1,70 +1,27 @@
-import 'normalize.css';
-
-import { MultiSelect, TreeSelect } from './components';
-import './style.scss';
-
-/** Region **/
-
-const regionSelect = document.querySelector<HTMLSelectElement>(
-    'select[name="region"]'
-);
-
-new MultiSelect(regionSelect, {
-    onChange: (selectedOptions) => {
-        console.log('regionSelect -> selectedOptions:', selectedOptions);
-    },
-});
-
-regionSelect?.addEventListener('change', (event) => {
-    const target = event.target as HTMLSelectElement;
-
-    console.log('regionSelect -> value:', target.value);
-});
-
-/** Customer **/
-
-const customerSelect = document.querySelector<HTMLSelectElement>(
-    'select[name="customer"]'
-);
-
-new TreeSelect(customerSelect, {
-    onChange: (selectedOptions) => {
-        console.log('customerSelect -> selectedOptions:', selectedOptions);
-    },
-});
-
-customerSelect?.addEventListener('change', (event) => {
-    const target = event.target as HTMLSelectElement;
-
-    console.log('customerSelect -> value:', target.value);
-});
-
-//my code 
-
-const form = document.querySelector<HTMLElement>('.form__back')!;
-const table = document.querySelector<HTMLInputElement>('.first')!;
-const table2 = document.querySelector<HTMLInputElement>('.second')!;
-const modal = document.querySelector<HTMLElement>('#modals')!;
-const option = document.querySelectorAll<HTMLInputElement>('.select')!;
-const select = document.querySelector<HTMLElement>('#selected')!;
-const reset = document.querySelector<HTMLElement>('.btn__filter')!;
-const open2 = document.querySelector<HTMLElement>('.a__top')!;
-const op = document.querySelector<HTMLElement>('.a__bottom')!;
-const accept = document.querySelector<HTMLElement>('.close')!;
-const accept2 = document.querySelector<HTMLElement>('.close1')!;
-const close = document.querySelector<HTMLElement>('.content__back__link')!;
-const result = document.querySelector<HTMLElement>('.top')!;
-const result1 = document.querySelector<HTMLElement>('.bottom')!;
-const search = document.querySelector<HTMLInputElement>('.content__back__input')!;
+const form = document.querySelector('.form__back');
+const table = document.querySelector('.first');
+const table2 = document.querySelector('.second');
+const modal = document.querySelector('#modals');
+const option = document.querySelectorAll('.select');
+const select = document.querySelector('#selected');
+const reset = document.querySelector('.btn__filter');
+const open2 = document.querySelector('.a__top');
+const op = document.querySelector('.a__bottom');
+const accept = document.querySelector('.close');
+const accept2 = document.querySelector('.close1');
+const close = document.querySelector('.content__back__link');
+const result = document.querySelector('.top');
+const result1 = document.querySelector('.bottom');
+const search = document.querySelector('.content__back__input');
 
 
 form.addEventListener('submit', function (e) {
-    return e.preventDefault() ;
+    return e.preventDefault();
 });
 
 table.addEventListener('click', function () {
     let arr = table.value.split(',');
-    for (let opt of Array.from(option)) {
+    for (let opt of option) {
         if (arr.includes(opt.value)) {
             opt.classList.add('selectfull');
             opt.classList.remove('select');
@@ -75,7 +32,7 @@ table.addEventListener('click', function () {
 
 table2.addEventListener('click', function () {
     let arr = table2.value.split(',');
-    for (let opt of Array.from(option)) {
+    for (let opt of option) {
         if (arr.includes(opt.value)) {
             opt.classList.add('selectfull');
             opt.classList.remove('select');
@@ -94,7 +51,7 @@ open2.addEventListener('click', function () {
 
 close.addEventListener('click', function () {
     modal.style.display = 'none';
-    for (let opt of Array.from(option)) {
+    for (let opt of option) {
         if (opt.className == 'selectfull') {
             opt.classList.add('select');
             opt.classList.remove('selectfull');
@@ -115,7 +72,7 @@ window.addEventListener('click', function (e) {
 //     opt.prepend(check);
 // }
 
-select.addEventListener('click', function (e: any) {
+select.addEventListener('click', function (e) {
     // let optionArr = Array.from(option);
     if (e.target.classList == 'select') {
         e.target.classList.add('selectfull');
@@ -136,40 +93,42 @@ select.addEventListener('click', function (e: any) {
     e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
 });
 
-accept.addEventListener('click', function () {
+accept.addEventListener('click', function (e) {
     let a = [];
-    for (let opt of Array.from(option)) {
+    for (let opt of option) {
         if (opt.className == 'selectfull') {
             a.push(opt.value);
         }
         let d = a.length;
-        if (d > 1) {
-            (result as any).text = `Показать выбранное (${d})`;
+        if (d < 1) {
+            result.text = 'Ничего не выбрано';
+        } else {
+            result.text = `Показать выбранное (${d})`;
         }
     }
     let c = a.join(',');
     table.value = c;
 });
 
-accept2.addEventListener('click', function () {
+accept2.addEventListener('click', function (e) {
     let a = [];
-    for (let opt of Array.from(option)) {
+    for (let opt of option) {
         if (opt.className == 'selectfull') {
             a.push(opt.value);
         }
         let d = a.length;
 
         if (d > 1) {
-            (result1 as any).text = `Показать выбранное (${d})`;
+            result1.text = `Показать выбранное (${d})`;
         }
         let c = a.join(',');
         table2.value = c;
     }
 });
 
-result.addEventListener('click', function () {
+result.addEventListener('click', function (e) {
     let arr = table.value.split(',');
-    for (let opt of Array.from(option)) {
+    for (let opt of option) {
         if (arr.includes(opt.value)) {
             opt.classList.add('selectfull');
             opt.classList.remove('select');
@@ -178,9 +137,9 @@ result.addEventListener('click', function () {
     modal.style.display = 'block';
 });
 
-result1.addEventListener('click', function () {
+result1.addEventListener('click', function (e) {
     let arr = table2.value.split(',');
-    for (let opt of Array.from(option)) {
+    for (let opt of option) {
         if (arr.includes(opt.value)) {
             opt.classList.add('selectfull');
             opt.classList.remove('select');
@@ -216,7 +175,7 @@ search.addEventListener('input', function () {
     }
 });
 
-function insertMark(str: string, pos: number, len: number) {
+function insertMark(str, pos, len) {
     return (
         str.slice(0, pos) +
         '<mark>' +
@@ -226,13 +185,12 @@ function insertMark(str: string, pos: number, len: number) {
     );
 }
 
-reset.addEventListener('click', function () {
+reset.addEventListener('click', function (e) {
     search.value = '';
-    for (let opt of Array.from(option)) {
+    for (let opt of option) {
         if (opt.className == 'selectfull') {
             opt.classList.add('select');
             opt.classList.remove('selectfull');
         }
     }
 });
-
